f6c4c5f5d028fc3bd16691d058c2a0969cd02120
unknown <manoela.marie@gmail.com>
2022-04-25 15:49:29 -0300

    Versao 01: Verificacao de login utilizando credenciais validas e invalidas. Verificacao de mensagens feita com o comando 'contain'

---
+++ cypress/integration/LoginEbac.spec.js
@@ -0,0 +1,52 @@
+///<reference types="cypress" />
+/*
+DocTeste({
+    Rotina: Login
+    Objetivo: Validar toda a rotina de login, considerando login com sucesso, senha invalida e email invalido
+    TelaPrincipal: Home|Minha conta 
+    PreRequisitos: Ter um usuário já cadastrado na plataforma - Usuario utilizado:aluno_ebac@teste.com 
+    Descricao: Acesso a tela de Minha conta, informado login valido e senha valida, verificado acesso a tela de usuário logado.
+               Acesso a tela de Minha conta, informado login valido e senha incorreta, verificado acesso negado com mensagem de aviso.
+               Acesso a tela de Minha conta, informado login inválido e senha válida, verificado acesso negado com mensagem de aviso.               
+    ResultadoEsperado: O sistema só deverá permitr login caso o usuário e a senha esteja já pré cadastrados
+    AnalistaOrigem: Manoela Marie Mulhmann
+})
+*/  
+
+
+//contexto do teste ('string entre aspas simples' inicio de função)*/
+context('Funcionalidade Login', () => {
+    
+    //estrutura do cenário
+    it('Deve fazer login com sucesso', () =>{
+
+        //comando visit acessa o link informado
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuario invalido', ()=>{
+
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno.ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir senha invalida', ()=>{
+
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste..com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+})
